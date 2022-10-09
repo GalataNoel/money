9582f56154f98308fdc8dde68afe6cc68119bcd2
Galata Noel <33789400+GalataNoel@users.noreply.github.com>
2022-10-09 14:59:49 +0200

    Add files via upload
    progtech 4.hét feladat

---
+++ main/java/money/Bank.java
@@ -0,0 +1,13 @@
+package money;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+/**
+ * ...
+ */
+public interface Bank {
+
+    BigDecimal getExchangeRate(Currency fromCurrency, Currency toCurrency);
+
+}
